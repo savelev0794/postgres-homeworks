122189d7a9be6021e7e22bc266baa44a770bae62
Egor Savelyev <savelev0794@mail.ru>
2023-11-01 23:35:57 +0500

    add main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,20 +1,43 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 
 import psycopg2
+import os
+import csv
 
-# connect to db
-conn = psycopg2.connect(host='localhost', database='test', user='postgres', password='0707')
-try:
-    with conn:
+CSV_DATA = os.path.join('north_data')
 
-        with conn.cursor() as cur:
-            # execute query
-            cur.execute('INSERT INTO user_account VALUES (%s, %s)', (6, 'Jake'))
 
-            cur.execute('SELECT * FROM user_account')
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+def csv_to_postgres(csv_data, csv_file, table_name):
+    try:
+        with psycopg2.connect(
+                host="localhost",
+                database="north",
+                user="postgres",
+                password="0707"
+        ) as conn:
+            with conn.cursor() as cur:
 
-finally:
-    conn.close()
+                with open(f"{csv_data}/{csv_file}", 'r') as file:
+                    reader = csv.reader(file)
+                    next(reader)  # Пропускаем заголовки
+
+                    for row in reader:
+                        # Вставляем данные в таблицу
+                        insert_query = f"INSERT INTO {table_name} VALUES ({', '.join(['%s'] * len(row))})"
+                        cur.execute(insert_query, row)
+                        conn.commit()
+
+                cur.execute(f"SELECT * FROM {table_name}")
+                rows = cur.fetchall()
+
+                for row in rows:
+                    # Распечатываем таблицу в консоли
+                    print(row)
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    csv_to_postgres(CSV_DATA, 'customers_data.csv', 'customers')
+    csv_to_postgres(CSV_DATA, 'employees_data.csv', 'employees')
+    csv_to_postgres(CSV_DATA, 'orders_data.csv', 'orders')
