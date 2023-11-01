2e01a32cdd65f652f33938a3a8f7365d19609833
Egor Savelyev <savelev0794@mail.ru>
2023-11-01 23:11:43 +0500

    add sql-code for creating tables and pyproject.toml

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar (50) NOT NULL,
+	last_name varchar (50) NOT NULL,
+	title varchar (250),
+	birthdate date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar (5) PRIMARY KEY,
+	company_name varchar (50) NOT NULL,
+	contact_name varchar (50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar (50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,20 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+
+# connect to db
+conn = psycopg2.connect(host='localhost', database='test', user='postgres', password='0707')
+try:
+    with conn:
+
+        with conn.cursor() as cur:
+            # execute query
+            cur.execute('INSERT INTO user_account VALUES (%s, %s)', (6, 'Jake'))
+
+            cur.execute('SELECT * FROM user_account')
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+finally:
+    conn.close()
